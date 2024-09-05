45642478f33a94e19901294ef946a9605f5b334a
genvalen <genvalen@protonmail.com>
2024-09-05 18:14:13 -0400

    Add db session to endpoint.

--- app/app.py
+++ app/app.py
@@ -1,13 +1,17 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel
+from sqlalchemy.orm import Session
+from . import models
+from .database import engine, get_db
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
 class Credentials(BaseModel):
     password: str
     ip_address: str
 
 @app.get("/v1/password/")
-def generate_password():
+def generate_password(db: Session = Depends(get_db)):
     rand_password = "password"
     return rand_password
