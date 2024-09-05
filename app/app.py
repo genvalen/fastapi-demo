310fa4571a2aa56ed0c171cda88428ad3c153f4a
genvalen <genvalen@protonmail.com>
2024-09-05 18:26:49 -0400

    Add psycopg connection.

--- app/app.py
+++ app/app.py
@@ -3,6 +3,9 @@ from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from . import models
 from .database import engine, get_db
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -10,6 +13,20 @@ app = FastAPI()
 class Credentials(BaseModel):
     password: str
     ip_address: str
+
+
+while True:
+    try:
+        conn = psycopg2.connect(host='localhost', dbname='fastapi', user='postgres', \
+            password="password", cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("Database connection was successful.")
+        break
+    except Exception as error:
+        print("the database connection failed.")
+        print(f"The error was: {error}")
+        time.sleep(2)
+
 
 @app.get("/v1/password/")
 def generate_password(db: Session = Depends(get_db)):
