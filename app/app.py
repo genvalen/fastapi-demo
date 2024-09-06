c2845faaa69efe43f0eedb2e72dbb02ef2201b2d
genvalen <genvalen@protonmail.com>
2024-09-05 23:34:33 -0400

    Add minimal html/styling to index page.

--- app/app.py
+++ app/app.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Request, Depends
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 import psycopg2
@@ -12,13 +15,17 @@ from .password_generator import password_generator
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
 class Credentials(BaseModel):
     password: str
     ip_address: str
 
 while True:
     try:
-        conn = psycopg2.connect(host='localhost', dbname='fastapi', user='postgres', \
+        conn = psycopg2.connect(host='localhost', dbname='demo', user='postgres', \
             password="password", cursor_factory=RealDictCursor)
         cursor = conn.cursor()
         print("Database connection was successful.")
@@ -28,9 +35,9 @@ while True:
         print(f"The error was: {error}")
         time.sleep(2)
 
-@app.get("/")
-async def root():
-    return {"message": "Welcome to my Fastapi password generator!"}
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse(request=request, name="index.html")
 
 @app.get("/v1/password/")
 async def get_password(db: Session = Depends(get_db)):
