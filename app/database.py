0b2e200d9412223bbed245352f2118b7e374da1b
genvalen <genvalen@protonmail.com>
2024-09-10 08:34:37 -0400

    Adapt .env for testing locally.

--- app/database.py
+++ app/database.py
@@ -4,9 +4,7 @@ from sqlalchemy.orm import sessionmaker
 from decouple import config
 import os
 
-database_url = os.getenv('DATABASE_URL', '').replace('postgres://', 'postgresql://') #specifc to heroku
-
-SQLALCHEMY_DATABASE_URL = config(database_url)
+SQLALCHEMY_DATABASE_URL = config('DATABASE_URL')
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
