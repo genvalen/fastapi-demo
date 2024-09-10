273a82a631b88ce0622c3e578a3bbca89c115439
genvalen <genvalen@protonmail.com>
2024-09-09 23:26:20 -0400

    update database variable in source code.

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from decouple import config
 
-SQLALCHEMY_DATABASE_URL = config('DB_URL')
+SQLALCHEMY_DATABASE_URL = config('DATABASE_URL')
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
