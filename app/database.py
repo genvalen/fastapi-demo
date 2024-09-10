5c68a462db10d1584ccfad82b59ec5c6ea48b937
genvalen <genvalen@protonmail.com>
2024-09-09 23:51:39 -0400

    Fix SQLAlchemy dialect issue and Procfile

--- app/database.py
+++ app/database.py
@@ -2,8 +2,11 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from decouple import config
+import os
 
-SQLALCHEMY_DATABASE_URL = config('DATABASE_URL')
+database_url = os.getenv('DATABASE_URL', '').replace('postgres://', 'postgresql://') #specifc to heroku
+
+SQLALCHEMY_DATABASE_URL = config(database_url)
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
